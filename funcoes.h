fa1bfe4b68e25c70a3090047f04d68c9449c67b0
Raphael Shodi <raphael.s.k@hotmail.com>
2024-03-24 00:39:05 -0300

    codigo otimizado e guardando valores no array, falta fazer rodar

--- funcoes.h
+++ funcoes.h
@@ -1,16 +1,15 @@
 /*
 00	  NOP   	     Nenhuma operação
 16	  STA    end	 Armazena acumulador no endereço “end” da memória
-32	  LDA    end	 Carrega o acumulador com o conteúdo do endereço “end”
-da memória 48	  ADD    end	 Soma o conteúdo do endereço “end” da memória ao
-acumulador 64	  OR     end	 Efetua operação lógica “OU” do conteúdo do
-endereço “end” da memória ao acumulador 80	  AND    end	 Efetua operação
-lógica “E” do conteúdo do endereço “end” da memória ao acumulador 96	  NOT
-Inverte todos os bits do acumulador 128	  JMP    end	 Desvio incondicional
-para o endereço “end” da memória 144	  JN     end	 Desvio condicional, se
-“N=1”, para o endereço “end” da memória 160	  JZ     end	 Desvio
-condicional, se “Z=1”, para o endereço “end” da memória 240	  HLT
-Para o ciclo de busca-decodificação-execução
+32	  LDA    end	 Carrega o acumulador com o conteúdo do endereço “end” da memória
+48	  ADD    end	 Soma o conteúdo do endereço “end” da memória ao acumulador
+64	  OR     end	 Efetua operação lógica “OU” do conteúdo do endereço “end” da memória ao acumulador
+80	  AND    end	 Efetua operação lógica “E” do conteúdo do endereço “end” da memória ao acumulador
+96	  NOT   	     Inverte todos os bits do acumulador
+128	  JMP    end	 Desvio incondicional para o endereço “end” da memória
+144	  JN     end	 Desvio condicional, se “N=1”, para o endereço “end” da memória
+160	  JZ     end	 Desvio condicional, se “Z=1”, para o endereço “end” da memória
+240	  HLT   	     Para o ciclo de busca-decodificação-execução
 */
 
 #ifndef FUNCOES_H
@@ -37,8 +36,10 @@ static int pegarValor()
     while ((verificar != 1))
     {
         while (getchar() != '\n')
-            ;                                                                  // limpar o buffer
-        printf("Erro de entrada. Por favor, insira um número válido.\nend: "); // reinsert number
+            ; // limpar o buffer
+        printf(
+            "Erro de entrada. Por favor, insira um número válido.\nend: "); // reinsert
+        // number
         verificar = scanf("%d", &i);
     }
     return i;
@@ -53,44 +54,18 @@ static void test(int dados[])
     printf("\n");
 }
 
-static void NOP(int dado[], int i) { dado[i] = 0; }
-
-static void STA(int dado[], int indice)
-{ // armazenar no acumulador
-    dado[indice] = 16;
-    printf("end: ");
-    int verificar = scanf("%d", &dado[indice + 1]);
-    while (verificar != 1)
-    {
-        while (getchar() != '\n')
-            ; // limpar o buffer
-        printf(
-            "Erro de entrada. Por favor, insira um número válido.\nend: "); // reincerir
-                                                                            // numero
-        verificar = scanf("%d", &dado[indice + 1]);
-    }
-}
-
-static void LDA(int dado[], int indice)
+static void NOP(int dado[], int i)
 {
-    dado[indice] = 32;
-    printf("end: ");
-    int verificar = scanf("%d", &dado[indice + 1]);
-    while (verificar != 1)
-    {
-        while (getchar() != '\n')
-            ; // limpar o buffer
-        printf(
-            "Erro de entrada. Por favor, insira um número válido.\nend: "); // reincerir
-                                                                            // numero
-        verificar = scanf("%d", &dado[indice + 1]);
-    }
+    dado[i] = 0;
 }
 
-// static void ADD(int dado[], int indice){
-//   dado[indice] = 48;
-//   dado[indice+1] = pegarValor();
-// }
+static int funcao(int dado[], int indice, int tipo)
+{
+    indice = pegarValor(); // pegar local onde vai guardar
+    dado[indice] = tipo;
+    dado[indice + 1] = pegarValor();
+    return indice + 2;
+}
 
 // Defina protótipos de outras funções do Neander aqui, se necessário
 
--- main.cpp
+++ main.cpp
@@ -4,15 +4,14 @@
 int main()
 {
     int dados[255];
-    bool inicio = true;
-    int expression;
     int i = 0;
     zerar(dados);
 
     do
     {
-        printf(" 1 - NOP\n 2 - STA\n 3 - LDA\n 4 - ADD\n 5 - OR\n 6 - AND\n 7 - NOT\n 8 - JMP\n 9 - JN\n 10 - JZ\n 11 - HLT\n 12 - Guardar dados\n 13 - run\n i:%d\n", i);
+        printf(" 1 - NOP\n 2 - STA\n 3 - LDA\n 4 - ADD\n 5 - OR\n 6 - AND\n 7 - NOT\n 8 - JMP\n 9 - JN\n 10 - JZ\n 11 - HLT\n 12 - Guardar dados\n 13 - run\n i:%d\n opção: ", i);
 
+        int expression;
         if (scanf("%d", &expression) != 1)
         {
             printf("Erro de entrada. Por favor, insira um número válido.\n");
@@ -32,30 +31,59 @@ int main()
             test(dados);
             break;
         case 1:
-            i = pegarValor(i);
+            i = pegarValor();
             NOP(dados, i);
-            i++;
             break;
         case 2:
-            i = pegarValor(i);
-            STA(dados, i);
-            i += 2;
+            // STA - Armazena o valor no endereço especificado
+            funcao(dados, i, 16);
             break;
         case 3:
-            i = pegarValor(i);
-            LDA(dados, i);
-            i += 2;
-        case 4:
-            // i = pegarValor(i);
-            // ADD(dados, i);
-        case 11:
-            printf("Instrução HLT - Saindo do programa.\n");
+            // LDA - Carrega o valor do endereço especificado
+            funcao(dados, i, 32);
             break;
+        case 4:
+            // ADD - Adiciona o valor ao acumulador
+            funcao(dados, i, 48);
+            break;
+        case 5:
+
+            // OR - Realiza a operação lógica OR
+            funcao(dados, i, 64);
+            break;
+        case 6:
+
+            // AND - Realiza a operação lógica AND
+            funcao(dados, i, 80);
+            break;
+        case 7:
+
+            // NOT - Realiza a operação lógica NOT
+            funcao(dados, i, 96);
+            break;
+        case 8:
+            // JMP - Salta para o endereço especificado
+            funcao(dados, i, 128);
+            break;
+        case 9:
+            // JN - Salta para o endereço especificado se o acumulador for negativo
+            funcao(dados, i, 144);
+            break;
+        case 10:
+            // JZ - Salta para o endereço especificado se o acumulador for zero
+            funcao(dados, i, 160);
+            break;
+        case 11:
+            // HLT - Encerra a execução do programa
+            i = pegarValor();
+            dados[i] = 240;
+            // printf("Instrução HLT - Saindo do programa.\n");
+            return 0;
         default:
             printf("Instrução inválida.\n");
         }
 
-    } while (inicio);
+    } while (1); // Continua indefinidamente até que o usuário selecione a opção de sair
 
     return 0;
 }
