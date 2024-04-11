d69d9cdbd65dfdd566c8a2025de2ff65f8a51789
Raphael Shodi <80370866+kosh202@users.noreply.github.com>
2024-04-11 18:40:29 +0000

    pegando numeros e guardado

---
+++ identificador/identificador.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "verificar.h"
+
+int main() {
+    // Definição do array de char
+    char meuArray[100] = "a12*+-s1/+25)("; 
+    int num[100];
+    int numInt= 0;
+    zerar(num);
+
+    printf("Digite uma palavra: %s\n", meuArray);
+
+  // numero(meuArray);//por enquanto estou só printando o numero e não guardando ele
+  // int quantNum = N(meuArray, num);
+  int index = extrairNumeros(meuArray, num);
+  prin(num, index);
+  // operacao(meuArray, '*');//mostrando no print e tem guardado nas posicoes do array
+  // operacao(meuArray, '/');
+  // operacao(meuArray, '+');
+  // operacao(meuArray, '-');
+  // operacao(meuArray, '(');
+  // operacao(meuArray, ')');
+
+
+  
+    return 0;
+}
---
+++ identificador/verificar.h
@@ -0,0 +1,64 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+#include <stdio.h>
+#include <iostream>
+// #include <ctype.h>
+
+void prin(int num[], int tamanho){
+  for (int i = 0; i < tamanho; i++)
+  {
+    printf("%d\n", num[i]);
+  }
+  
+}
+
+void zerar(int num[]){
+  for (int i = 0; i < 100; i++)
+  {
+    num[i] = 0;
+  }
+  
+}
+
+void limpar(char palavra[]) {
+  for (int i = 0; i < 100; i++) {
+    palavra[i] = '\0'; // Define cada elemento do array como um caractere nulo
+  }
+}
+
+int extrairNumeros(char arrChar[], int arrInt[]) {
+    int numInt = 0; // Inicializa o contador de números inteiros
+    // Itera sobre cada caractere na string
+    for (int i = 0; arrChar[i] != '\0'; ++i) {
+        // Verifica se o caractere atual é um dígito
+        if (isdigit(arrChar[i])) {
+            // Converte a substring contendo o número para inteiro e armazena no array de inteiros
+            arrInt[numInt++] = atoi(&arrChar[i]);
+            // Avança o índice para o próximo caractere não numérico
+            while (isdigit(arrChar[i]))
+                ++i;
+        }
+    }
+    return numInt;
+}
+
+static void operacao(char palavra[], char operacao) {
+  char local[100];
+  int indices[100];
+  int index = 0;
+  for (int i = 0; i < 100; i++) {
+    if (palavra[i] == operacao) {
+      local[i] = operacao;
+      indices[index] = i;
+      index++;
+    }
+  }
+  for (int i = 0; i < 100; i++) {
+    if (palavra[i] != '\0') {
+      printf("%c: %c\n", palavra[i], local[i]);
+    }
+  }
+}
+
+#endif
