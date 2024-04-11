843ed0d4af08ff21c8815071e5cebf2e8c525938
Raphael Shodi <80370866+kosh202@users.noreply.github.com>
2024-04-11 00:22:45 +0000

    funcionando o simulador

--- funcionando/leitura.h
+++ funcionando/leitura.h
@@ -35,30 +35,30 @@ int binarioParaDecimal(long long binario) {
 }
 
 //end é o terceiro numero que o ler arquivo pega
-void NOP(int PC, int AC, int end, int indice){
-    indice ++;
+void NOP(int &PC, int &AC){
+    PC ++;
 };
 
-void STA(int PC, int AC, int end, int indice, int memoria[]){
-    AC = memoria[end];
-    indice++;
+void STA(int &PC, int &AC, int memoria[]){
+    memoria[PC+1] = AC;
+    PC+=2;
 }
 
-void LDA(int PC, int AC, int end, int indice, int memoria[]){
-    memoria[end] = AC;
-    indice+=2;
+void LDA(int &PC, int &AC, int memoria[]){
+    AC = memoria[memoria[PC+1]];
+    PC+=2;
 }
 
-void ADD(int PC, int AC, int end, int indice, int memoria[]){
-    PC+= memoria[end];
-    indice+=2;
+void ADD(int &PC, int &AC, int memoria[]){
+    AC+= memoria[memoria[PC+1]];
+    PC+=2;
 }
 
-void OR(int PC, int AC, int end, int indice, int memoria[]){
+void OR(int &PC, int &AC, int memoria[]){
     int resultado = 0;
     int expoente = 0;
     int binario1 = decimalParaBinario(AC);
-    int binario2 = decimalParaBinario(memoria[end]);
+    int binario2 = decimalParaBinario(memoria[PC+1]);
     
     while (binario1 > 0 || binario2 > 0) {
         int bit1 = binario1 % 10;
@@ -69,15 +69,15 @@ void OR(int PC, int AC, int end, int indice, int memoria[]){
         binario2 /= 10;
         expoente++;
     }  
-    indice+=2;
+    PC+=2;
 
 }
 
-void AND(int PC, int AC, int end, int indice, int memoria[]) {
+void AND(int &PC, int &AC, int memoria[]) {
     int resultado = 0;
     int expoente = 0;
     int binario1 = decimalParaBinario(AC);
-    int binario2 = decimalParaBinario(memoria[end]);
+    int binario2 = decimalParaBinario(memoria[PC+1]);
 
     while (binario1 > 0 && binario2 > 0) {
         int bit1 = binario1 % 10;
@@ -88,11 +88,63 @@ void AND(int PC, int AC, int end, int indice, int memoria[]) {
         binario2 /= 10;
         expoente++;
     }
-    indice+=2;
+    PC+=2;
+}
+
+// Função para fazer a operação lógica NOT em um número binário
+void NOTBinario(int &PC, int &AC) {
+    int resultado = 0;
+    int expoente = 0;
+    int binario = decimalParaBinario(AC);
+    
+    while (binario > 0) {
+        int bit = binario % 10;
+        int bitNOT = (bit == 1) ? 0 : 1;
+        resultado += bitNOT * std::pow(10, expoente);
+        binario /= 10;
+        expoente++;
+    }
+    
+    // Se o resultado não tiver 8 dígitos, preencher com zeros à esquerda
+    while (expoente < 8) {
+        resultado += 0 * std::pow(10, expoente);
+        expoente++;
+    }
+    
+    AC = binarioParaDecimal(resultado);
+    PC++;
+}
+
+// Função para o salto incondicional JMP
+void JMP(int &PC, int memoria[]) {
+    PC = memoria[PC+1]; // Retorna o endereço para onde o PC deve ser pulado
+}
+
+// Função para o salto condicional se o AC for negativo (JN)
+void JN(int &PC, int &AC, int memoria[]) {
+    if (AC < 0) {
+        memoria[PC+1]; // Retorna o endereço para onde o PC deve ser pulado se AC for negativo
+    } else {
+        PC += 2; // Retorna o próximo endereço se AC for positivo ou zero
+    }
+}
+
+// Função para o salto condicional se o AC for zero (JZ)
+void JZ(int &PC, int &AC, int memoria[]) {
+    if (AC == 0) {
+        PC = memoria[PC+1]; // Retorna o endereço para onde o PC deve ser pulado se AC for zero
+    } else {
+        PC+=2; // Retorna o próximo endereço se AC for diferente de zero
+    }
+}
+
+// Função para a instrução HLT (halt)
+int HLT() {
+    return -1; // Retorna -1 para indicar que o programa deve ser interrompido
 }
 
 //leitura do arquivo depois implemntar as fuções para rodar o neader
-void lerArquivo(int i, int PC, int AC) {
+void lerArquivo(int instrucoes[]) {
     FILE *arquivo = fopen("../entrada.txt", "r"); // Abre o arquivo "entrada.txt" para leitura
     if (!arquivo) { // Verifica se o arquivo foi aberto com sucesso
         fprintf(stderr, "Erro ao abrir o arquivo.\n");
@@ -101,23 +153,79 @@ void lerArquivo(int i, int PC, int AC) {
 
     int primeiroNumero, segundoNumero, terceiroNumero;
     char instrucao[100]; // Uma string para armazenar a instrução
+    int i=0;
     while (fscanf(arquivo, "%d %d", &primeiroNumero, &segundoNumero) != EOF) {
         if ((segundoNumero == 0)|| (segundoNumero == 96) || (segundoNumero == 240)) {
             terceiroNumero = -1; // Define um valor padrão caso não haja terceiro número
-            
+            instrucoes[i]=segundoNumero;
+            i++;
         } else {
             fscanf(arquivo, "%d", &terceiroNumero);
+            instrucoes[i]=segundoNumero;
+            instrucoes[i+1]=terceiroNumero;
+            i+=2;
+
         }
         fscanf(arquivo, " %[^\n]", instrucao); // Lê a instrução
-        printf("Primeiro número: %d\n", primeiroNumero); // Imprime o primeiro número
-        printf("Segundo número: %d\n", segundoNumero); // Imprime o segundo número
+        // printf("Primeiro número: %d\n", primeiroNumero); // Imprime o primeiro número
+        // printf("Segundo número: %d\n", segundoNumero); // Imprime o segundo número
         if (terceiroNumero != -1) { // Se houver um terceiro número, imprime-o
-            printf("Terceiro número: %d\n", terceiroNumero);
+            // printf("Terceiro número: %d\n", terceiroNumero);
         }
-        printf("Instrução: %s\n", instrucao); // Imprime a instrução
+        // printf("Instrução: %s\n", instrucao); // Imprime a instrução
     }
 
     fclose(arquivo); // Fecha o arquivo
 }
+
+void rodar(int array[], int &PC, int &AC){
+
+    while (array[PC] != 240)
+    {
+        printf("PC: %d\n AC: %d\n", PC, AC);
+
+        switch(array[PC]) {
+        case 0:
+            NOP(PC,AC);
+            break;
+        case 16:
+            STA(PC,AC,array);
+            break;
+        case 32:
+            LDA(PC,AC,array);
+            break;
+        case 48:
+            ADD(PC,AC,array);
+            break;
+        case 64:
+            OR(PC,AC,array);
+            break;
+        case 80:
+            AND(PC,AC,array);
+            break;
+        case 96:
+            NOTBinario(PC, AC);
+            break;
+        case 128:
+            JMP(PC,array);
+            break;
+        case 144:
+            JN(PC,AC, array);
+            break;
+        case 160:
+            JZ(PC,AC,array);
+            break;
+        case 240:
+            HLT();
+            PC++;
+            break;
+        default:
+            printf("instrucao invalida");
+            break;
+            // code block
+        }
+    }
+} 
+
 
 #endif
--- funcionando/test.cpp
+++ funcionando/test.cpp
@@ -2,16 +2,39 @@
 #include <cstdio>
 #include "leitura.h"
 
+int tamanho = 255;
 
+void zera(int array[]){
+  for (int i = 0; i < tamanho; i++)
+    {
+      array[i] = 0;
+    }
+    
+}
 
+void lerArray(int array[]){
+  for (int i = 0; i < tamanho; i++)
+    {
+      printf("%d, ", array[i]);
+    }
+}
 
 int main() {
-    double i, PC, AC = 0;
-    // lerArquivo(i, PC, AC);
-      int bin = decimalParaBinario(3);
-    // int bin = decimalParaBinario(3);
-    // int dec = binarioParaDecimal(bin);
-    printf("%d\n",bin);
+    int AC = 0;
+    int PC = 0;
+    
+    int instrucoes[tamanho] = {0};
+    zera(instrucoes);
+    lerArquivo(instrucoes);
+    instrucoes[130] = 2;
+    instrucoes[131] = 4;
+    instrucoes[132] = 5;
+    lerArray(instrucoes);
+    
+    rodar(instrucoes,PC,AC);
+    printf("PC: %d\nAC: %d", PC, AC);
+
+    
 
 
     return 0;
