afd6fbc8bc853a2bd05a53159a65051edc5d99d6
Raphael Shodi <80370866+kosh202@users.noreply.github.com>
2024-04-12 05:30:05 +0000

    funcionado falta juntar tudo

--- identificador/identificador.cpp
+++ identificador/identificador.cpp
@@ -3,25 +3,35 @@
 
 int main() {
     // Definição do array de char
-    char meuArray[100] = "a12*+-s1/+25)("; 
+    char meuArray[100] = "5+2"; 
     int num[100];
+    int instrucoes[255];
     int numInt= 0;
     zerar(num);
+    zerar(instrucoes);
+    instrucoes[0]=32;//pegar item
+    int test[255];
+    zerar(test);
 
     printf("Digite uma palavra: %s\n", meuArray);
 
   // numero(meuArray);//por enquanto estou só printando o numero e não guardando ele
   // int quantNum = N(meuArray, num);
-  int index = extrairNumeros(meuArray, num);
-  prin(num, index);
-  operacao(meuArray, '*');//mostrando no print e tem guardado nas posicoes do array
+  int qtdValores = extrairNumeros(meuArray, num);
+  prin(num, qtdValores);
+  printf("\n");
+  //operacao(meuArray, '*');//mostrando no print e tem guardado nas posicoes do array
   // operacao(meuArray, '/');
-  // operacao(meuArray, '+');
+  int qtdSum = operacao(meuArray, '+');
+  int qtdInstrucoes = ((qtdSum*2)+2)+1;
   // operacao(meuArray, '-');
   // operacao(meuArray, '(');
   // operacao(meuArray, ')');
+  printf("quant instrucoes: %d\nquant valores: %d\n\n\n", qtdInstrucoes, qtdValores);
 
-
+extrairOperadores(meuArray, instrucoes);
+dados(qtdInstrucoes,qtdValores, instrucoes, num, test);
+prin(test, (qtdInstrucoes+qtdValores));
   
     return 0;
 }
--- identificador/verificar.h
+++ identificador/verificar.h
@@ -43,7 +43,7 @@ int extrairNumeros(char arrChar[], int arrInt[]) {
     return numInt;
 }
 
-static void operacao(char palavra[], char operacao) {
+int operacao(char palavra[], char operacao) {
   char local[100];
   limpar(local);
   int indices[100];
@@ -60,6 +60,53 @@ static void operacao(char palavra[], char operacao) {
       printf("%c: %c\n", palavra[i], local[i]);
     }
   }
+  return index;
 }
+
+void extrairOperadores(const char expressao[], int operadores[]) {
+    int j = 1; // Índice para operadores
+    
+    // Calcular o comprimento da string de entrada manualmente
+    int len = 0;
+    while (expressao[len] != '\0') {
+        len++;
+    }
+
+    // Percorrer a expressão caractere por caractere
+    for (int i = 0; i < len; i++) {
+        // Se o caractere atual for um operador, adicioná-lo ao array de operadores
+        // depois daria pla implementar outros
+        if (expressao[i] == '+') {
+            operadores[j] = 48;
+            j++;
+        }
+    }
+}
+
+//guarda o array para mandar para o simulador do neande (final[])
+void dados(int qtdInst, int qtdVal, int instrucoes[], int valores[], int final[]){
+  int index=0;
+  int iVal=0;
+  // instrucoes[1]=qtdInst;
+  for (int i = 0; i < (qtdInst+qtdVal); i++)
+  {
+   if (i<qtdInst-1)
+   {
+    final[i] = instrucoes[index];
+    i++;
+    final[i] = index+qtdInst;
+    index++;
+   }else if (i==(qtdInst-1))
+   {
+    final[i] = 240;
+   }else if(i>(qtdInst-1)){
+    final[i] = valores[iVal];
+    iVal++;
+   }
+  }
+
+}
+  
+
 
 #endif
